0bb3e3f95970661131b857ac47818c166346a922
Paragoumba <paragoumba@gmail.com>
2021-01-30 00:24:06 +0100

    Renamed file GameLogic.cpp to Game.cpp
    Renamed file GameLogic.cpp to Game.cpp to match the name of the class inside it

---
+++ src/Game.cpp
@@ -0,0 +1,23 @@
+#include "Game.hpp"
+
+Game::Game(){
+
+}
+
+void Game::init(){
+
+}
+
+void Game::input(){
+
+}
+
+void Game::update(){
+
+}
+
+void Game::render() const {
+
+}
+
+Game::~Game() = default;
